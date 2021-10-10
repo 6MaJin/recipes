f3e2a896cf61c314074687edb74fe722af9ede46
Six MaJin <c.kohlstruk@web.de>
2021-10-11 00:32:24 +0200

    Rezepte angelegt

---
+++ resources/views/recipe/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts/app')
+@section('content')
+    <div class="container">
+        <div class="row justifiy-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Alle Listen</div>
+                    <div class="card-body">
+                        <form action="/shoppinglist/{{$shoppinglist->id}}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input value="{{old('name') ?? $shoppinglist->name}}" type="text" class="form-control"
+                                       id="name" name="name">
+                            </div>
+                            <div class="form-group">
+                                <div id="sortable" class="product-list" data-id="{{$shoppinglist->id}}">
+                                    @foreach($shoppinglist->products()->orderBy('product_shoppinglist.sort','ASC')->get() as $product)
+                                        <div class="btn btn-outline-secondary btn-sm mt-1 ui-sortable-handle"
+                                             data-id={{$product->id}}>{{$product->name}}</div>
+                                    @endforeach
+                                </div>
+                            </div>
+
+                            {{$product -> sortedProducts}}
+
+                            <div class="mb-3">
+                                <div class="livesearch-container">
+                                    <livewire:productfinder :shoppinglist="$shoppinglist"/>
+                                </div>
+
+                            </div>
+
+                            <div style="clear:both"></div>
+                            <div class="form-group">
+                                <label for="note">Notes</label><br/>
+                                <textarea name="note" id="note" cols="30"
+                                          rows="10">{{old('note') ?? $shoppinglist->note}}</textarea>
+                            </div>
+                            <button class="btn btn-primary" type="submit"><i class="fa fa-save"></i></button>
+                        </form>
+                        <a class="btn btn-secondary float-right" href="{{ URL::previous() }}"><i
+                                class="fa fa-arrow-circle-up"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('after_script')
+    <script>
+        function addProduct(product_id, product_name) {
+            if ($('.product-list').find("[data-id=" + product_id + "]").length === 0) {
+                $('.product-list').append('<div class="btn btn-outline-secondary btn-sm mt-1">' + product_name + '</div>').append("terter");
+
+            }
+        }
+
+        function ajaxStore() {
+            let product_name = $("#add_product").val();
+            $.ajax({
+                method: "POST",
+                dataType: 'json',
+                url: "{{route('product.ajax-store')}}",
+                data: {_token: "{{ csrf_token() }}", name: product_name, shoppinglist_id: {{ $shoppinglist->id }}}
+            })
+                .done(function (data) {
+                    console.log(data);
+                    if (data.status == 'success') {
+                        $('.product-list').append('<div class="btn btn-outline-secondary btn-sm mt-1" onclick="removeProduct(' + data.product_id + ')" data-id="' + data.product_id + '">' + data.product_name + '</div>');
+                    }
+                });
+        }
+
+        function removeProduct(product_id) {
+            return;
+            $('.product-list').find("[data-id=" + product_id + "]").remove();
+        }
+    </script>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('st
 Route::resource('shoppinglist', 'App\Http\Controllers\ShoppinglistController')/*->middleware('auth')*/;
 Route::resource('user', 'App\Http\Controllers\UserController')/*->middleware('auth')*/;
 Route::resource('product', 'App\Http\Controllers\ProductController');
+Route::resource('recipe', 'App\Http\Controllers\RecipeController');
 Route::post('/product/ajax-store','App\Http\Controllers\ProductController@ajaxStore')->name('product.ajax-store');
 
 Route::post('/shoppinglist/{shoppinglist_id}/update-order','App\Http\Controllers\ShoppinglistController@updateOrder')->name('shoppinglist.update-order');
