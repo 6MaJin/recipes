0802172718e4624a425bec852642df38700c5b89
Six MaJin <c.kohlstruk@web.de>
2021-10-21 10:37:35 +0200

    admin funzt immer noch nicht aus GRÜNDEN

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -21,21 +21,7 @@ class RedirectIfAuthenticated
     {
         $guards = empty($guards) ? [null] : $guards;
 
-
-        /*if (Auth::guard('admin')->check()) {
-            return redirect('/admin');
-        }*/
-
-
-        /*if (Auth::guard('admin')->check()) {
-            return redirect('/admin');
-        }*/
-
-
         foreach ($guards as $guard) {
-
-
-
             if (Auth::guard($guard)->check()) {
                 return redirect(RouteServiceProvider::HOME);
             }
@@ -43,5 +29,4 @@ class RedirectIfAuthenticated
 
         return $next($request);
     }
-
 }
