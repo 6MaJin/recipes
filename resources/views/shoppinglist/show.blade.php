9255455b82db166f68c96397925b648cf8290d45
Christ <c.kohlstruk@web.de>
2021-12-10 20:41:16 +0100

    Show funzt fast

--- resources/views/shoppinglist/show.blade.php
+++ resources/views/shoppinglist/show.blade.php
@@ -1,32 +1,43 @@
-@extends('layouts.app')
+@extends('layouts/app')
 @section('content')
-
-
-
     <div class="container">
-        <div class="card-header"><h3>{{$shoppinglist->name}}</h3></div>
-        <div class="card-body">
-            <img class="img-fluid" src="{{URL::asset($shoppinglist->bild)}}" alt="">
+        <div class="row">
+            <div class="col-md-12 justify-content-center d-inline-flex">
+                <div class="card">
+                    <div class="card-header"><h3>{{$shoppinglist->name}}</h3></div>
+                    <div class="card-body">
 
+                        <div class="form-group">
+                            <div id="sortable" class="product-list text-center" data-id="{{$shoppinglist->id}}">
 
-            <div class="form-group">
-                <div id="sortable" class="product-list text-center" data-id="{{$shoppinglist->id}}">
-
-
-                    @foreach($products AS $product)
-                        <div id="product_{{ $product->id }}"
-                             class="btn btn-primary btn-sm mt-1 ui-sortable-handle mr-auto ml-auto shoplist">{{$product->pivot->count ."x ". $product->name}}
-                            <button type="button" onclick="removeProduct({{$product->id}})"
-                                    class="float-right btn btn-danger btn-sm ml-1"
-                                    data-id={{$product->id}}><i
-                                    class="fa fa-minus"> </i></button>
+                                @foreach($products AS $product)
+                                    <div>
+                                        <div id="product_{{ $product->id }}"
+                                             class=" pl-show btn btn-primary btn-sm mt-1 ui-sortable-handle">{{$product->pivot->count."x ". $product->name}}
+                                        </div>
+                                        <button type="button" onclick="removeProduct({{$product->id}})"
+                                                class="delete-product-button position-relative btn btn-outline-danger btn-sm"
+                                                data-id={{$product->id}}><i
+                                                class="fa fa-minus"> </i></button>
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
-                    @endforeach
+
+
+                        <div class="mb-3">
+                            <div class="container<!--livesearch-container-->">
+                                {{--                                    <livewire:productfinder :shoppinglist="$shoppinglist"/>--}}
+                                <input type="text" id="add_product" value="">
+                                <button class="btn btn-outline-secondary" type="button" id="button-addon2"
+                                        onclick="ajaxStoreProduct()"><i class="fa fa-plus"></i> Produkt hinzufügen
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="flex-column" id="">{{$shoppinglist->note}}</div>
         </div>
-
     </div>
 
 
@@ -45,21 +56,25 @@
                     _token: "{{ csrf_token() }}",
                     name: product_name,
                     shoppinglist_id: {{ $shoppinglist->id }},
-                }
-            })
-                .done(function (data) {
-                    console.log(data);
-                    if (data.status == 'success') {
-                        $('.product-list').append('<div id="product_' + data.product_id + '" class="btn btn-outline-success btn-sm mt-1" data-list_id="' + data.shoppinglist_id + '"  data-id="' + data.product_id + '">' + data.product_name + '<i onclick="removeProduct(' + data.product_id + ')" class="float-right btn-sm btn btn-outline-danger fa fa-minus"></i></div>');
+                },
+                success: function (data) {
+                    $('.ajax-alert').removeClass('d-none').text(data.message);
+                    if ($('#product_' + data.product_id).length) {
+
+                            $('#product_' + data.product_id).text(data.count + 'x ' + data.product_name);
+
+                    } else {
+                        $('.product-list').append('<div class="ui-sortable-handle"><div id="product_' + data.product_id + '" class="position-relative btn btn-outline-success btn-sm mt-1 ui-sortable-handle">' + data.count + 'x ' + data.product_name + '</div><button type="button" onclick="removeProduct(' + data.product_id + ')" class="delete-product-button position-relative btn btn-outline-danger btn-sm" data-id=' + data.product_id + '><i class="fa fa-minus"> </i></button></div>');
                     }
-
-                });
+                },
+                error: function (response) {
+                    console.log('Error:', response);
+                },
+            });
         }
-
 
         function removeProduct(product_id) {
             console.log('product_id:' + product_id);
-            $('#product_' + product_id).remove();
             $.ajax({
                 method: "POST",
                 url: "/shoppinglist/ajax-delete-product",
@@ -69,14 +84,21 @@
                     product_id: product_id
                 },
                 success: function (data) {
-                    $('.ajax-alert').text(data['message']);
+
+                    if (data.count > 0) {
+                        $('#product_' + data.product_id).text(data.count + 'x ' + data.product_name);
+                    } else {
+                        $('#product_' + product_id).parent().remove();
+                    }
+
+
+                    $('.ajax-alert').removeClass('d-none').text(data['message']);
                 },
                 error: function (response) {
                     console.log('Error:', response);
                 }
             });
             return false;
-
         }
     </script>
 @endsection
