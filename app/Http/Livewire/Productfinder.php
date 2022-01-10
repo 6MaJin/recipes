33a780ce937a18fc61dc11f3991145939d0be00a
Six MaJin <c.kohlstruk@web.de>
2022-01-10 01:11:13 +0100

    livewire product search only users products

--- app/Http/Livewire/Productfinder.php
+++ app/Http/Livewire/Productfinder.php
@@ -3,10 +3,13 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use App\Models\Shoppinglist;
+use App\Models\User;
 use Illuminate\Support\Collection;
 use Livewire\Component;
 use PhpParser\ErrorHandler\Collecting;
-
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 class Productfinder extends Component
 {
     public $search;
@@ -15,7 +18,18 @@ class Productfinder extends Component
     public function render()
     {
         if(strlen($this->search)>1) {
-            $products = Product::where('name', 'LIKE', '%' . $this->search . '%')->get();
+            $user = Auth::user();
+            /*$products = Product::where('name', 'LIKE', '%' . $this->search . '%')->get();*/
+
+            /*$products = Product::where('name', 'LIKE', '%' . $this->search . '%')
+                ->where('products.id', '=', 'product_shoppinglist.product_id')
+                ->get();*/
+            $products = DB::table('users')
+                ->join('shoppinglists', 'users.id', '=', 'shoppinglists.user_id')
+                ->join('product_shoppinglist', 'shoppinglists.id', '=', 'product_shoppinglist.shoppinglist_id')
+                ->join('products', 'products.id', '=', 'product_shoppinglist.product_id')
+                ->where('users.id', '=', $user->id)
+                ->get();
         } else {
             $products = Array();
         }
